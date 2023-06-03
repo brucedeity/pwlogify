11e53a17378819213c35f15f1bb4c0523cc5444c
brucedeity <bruce.pedro05@gmail.com>
2023-06-03 17:03:52 -0300

    Updated composer.json description

--- scripts/world2_listener.sh
+++ scripts/world2_listener.sh
@@ -2,13 +2,9 @@
 
 server_path="/home"
 
-# Get the directory of the current script
 script_dir=$(dirname "$(readlink -f "$0")")
-
-# Determine the path to Logify.php relative to the script directory
 logify_script="${script_dir}/../app/Logify.php"
 
-# Function to process log lines
 process_log_line() {
     local log_line=$1
     php_output=$(php "${logify_script}" "${log_line}" 2>> "${script_dir}/../logs/errors.txt")
@@ -16,7 +12,6 @@ process_log_line() {
 }
 
 
-# Monitor and process log lines
 stdbuf -oL tail -f -n0 \
     "${server_path}/logs/world2.formatlog" \
     "${server_path}/logs/world2.log" \
